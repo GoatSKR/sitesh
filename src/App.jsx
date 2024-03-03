5ef51d891a8bd8b754d6d250c26324fd024703c2
GoatSKR <sitesh.raulo.pj@gmail.com>
2024-03-03 06:22:34 +0530

    3D Modelling added, Hero About Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-
-import { Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import ContactUs from "./pages/ContactUs";
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 function App() {
   return (
-    <Routes>
-      <Route exact path="/*" element={<Home />} />
-      <Route exact path="/contact" element={<ContactUs />} />
-    </Routes>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary '>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center '>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+
+        </div>
+      </div>
+
+    </BrowserRouter>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import AnimatedCursor from "../components/AnimatedCursor";
+
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import SocialIcons from "../components/SocialIcons";
+import ParticleBackground from "../components/Particle";
 export default function Home() {
     return (
         <>
+            <ParticleBackground />
             <AnimatedCursor />
+            
             <Header />
             <Hero />
             <SocialIcons />
