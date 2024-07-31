b623ef57f6045ed4b3a11e448beca593714aafa9
GoatSKR <sitesh.raulo.pj@gmail.com>
2024-07-31 23:37:07 +0530

    grammar error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Sitesh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a introvert and shy coder, <br className='sm:block hidden' />
+            I am an introvert and shy coder, <br className='sm:block hidden' />
             I Develop Frontend and Backend Solutions.
           </p>
         </div>
